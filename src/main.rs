cdc48882ed850ac1963a38756175b8ca54b73f9c
Alexandr Krylov <alexandr.krylov@gmail.com>
2019-07-01 18:55:24 +0600

    begin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{:?} ", avg_med_mod(vec![1, 2, 3, 4, 5, 6, 7, 8, 9]));
+}
+
+fn avg_med_mod(vec: Vec<i32>) -> (f32 , f32, i32) {
+    let mut avg = 0.0;
+    (2.4, 12.0, 22)
 }
